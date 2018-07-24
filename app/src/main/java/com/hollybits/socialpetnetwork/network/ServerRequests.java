4aeb76eeba2b317708135cfb1ed137c5496517b3
VictorYezhov <vicyezh@gmail.com>
2018-07-24 17:06:30 +0300

    login page

---
+++ app/src/main/java/com/hollybits/socialpetnetwork/network/ServerRequests.java
@@ -0,0 +1,13 @@
+package com.hollybits.socialpetnetwork.network;
+
+/**
+ * Created by Victor on 24.07.2018.
+ */
+
+public interface ServerRequests {
+
+    String BASE_REMOTE_OUR = "http://206.189.61.135:8080/";
+    String BASE_LOCKAL = "http://10.0.2.2:8080/";
+
+
+}
