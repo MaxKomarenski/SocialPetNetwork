a046c91c51110ba228d3b080c20912a3031e5352
MaxKomarenski <komarenski@ucu.edu.ua>
2018-07-24 17:13:01 +0300

    new

--- app/src/main/java/com/hollybits/socialpetnetwork/Fragments/Account.java
+++ app/src/main/java/com/hollybits/socialpetnetwork/Fragments/Account.java
@@ -22,6 +22,7 @@ public class Account extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
+
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
